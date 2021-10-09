23c89b8d7a4e49e1743566019d03c2913cc5a41e
Ivan Ivanov <ivanov.ivan.v@gmail.com>
2021-10-09 23:53:48 +0300

    refactor: - refactor setup flags and flags parse into setParseFlags fun - refactor validation code into separate function

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
+	"io"
 	"os"
 )
 
@@ -13,10 +15,7 @@ type ProjectConfiguration struct {
 	staticAssets bool
 }
 
-var errors []string
-
-
-func main() {
+func setupParseFlags(w io.Writer, args []string) ProjectConfiguration{
 	config := ProjectConfiguration{}
 	flag.StringVar(&config.projectName, "n", "", "Project name")
 	flag.StringVar(&config.locationOnDisk, "d", "", "Project location on disk")
@@ -24,18 +23,38 @@ func main() {
 	flag.BoolVar(&config.staticAssets, "s", false, "Project will have static assets or not")
 	flag.Parse()
 
-	if config.projectName == "" {
-		errors = append(errors, "Project name cannot be empty")
+	// fs := flag.NewFlagSet("scaffold-gen", flag.ContinueOnError)
+	// err := fs.Parse(args)
+
+	// if fs.NArg() != 0 {
+	// 	return config, errors.New("No positional parameters expected")
+	// }
+	// return config, err
+	return config
+}
+
+func validateConf(config *ProjectConfiguration) []error {
+	var validationErrors []error
+
+	if len(config.projectName) == 0 {
+		validationErrors = append(validationErrors, errors.New("Project name cannot be empty"))
 	}
-	if config.locationOnDisk == "" {
-		errors = append(errors, "Project location cannot be empty")
+	if len(config.locationOnDisk) ==  0 {
+		validationErrors = append(validationErrors, errors.New("Project location cannot be empty"))
 	}
-	if config.remoteRepoUrl == "" {
-		errors = append(errors, "Project repository URL cannot be empty")
+	if len(config.remoteRepoUrl) == 0 {
+		validationErrors = append(validationErrors, errors.New("Project repository URL cannot be empty"))
 	}
 
-	if (len(errors) > 0) {
-		for _ , error := range errors {
+	return validationErrors
+}
+
+func main() {
+	config := setupParseFlags(os.Stdout, os.Args[1:])
+	validationErrors := validateConf(&config)
+
+	if (len(validationErrors) > 0) {
+		for _ , error := range validationErrors {
 			fmt.Printf("%s\n", error)
 		}
 		os.Exit(1)

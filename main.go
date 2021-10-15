8d577a0ed8d8aa3fbba640da4e0cf05c1850fbf2
Ivan Ivanov <ivanov.ivan.v@gmail.com>
2021-10-16 00:31:29 +0300

    refactor/test: - refactor function setupParseFlags - add unit tests

--- main.go
+++ main.go
@@ -9,54 +9,68 @@ import (
 )
 
 type ProjectConfiguration struct {
-	projectName string
-	locationOnDisk string
-	remoteRepoUrl string
-	staticAssets bool
+	Name string
+	LocalPath string
+	RepoUrl string
+	StaticAssets bool
 }
 
-func setupParseFlags(w io.Writer, args []string) ProjectConfiguration{
+func setupParseFlags(w io.Writer, args []string) (ProjectConfiguration, error){
 	config := ProjectConfiguration{}
-	flag.StringVar(&config.projectName, "n", "", "Project name")
-	flag.StringVar(&config.locationOnDisk, "d", "", "Project location on disk")
-	flag.StringVar(&config.remoteRepoUrl, "r", "", "Project remote repository")
-	flag.BoolVar(&config.staticAssets, "s", false, "Project will have static assets or not")
-	flag.Parse()
+	fs := flag.NewFlagSet("scaffold-gen", flag.ContinueOnError)
+	fs.SetOutput(w)
+	fs.StringVar(&config.Name, "n", "", "Project name")
+	fs.StringVar(&config.LocalPath, "d", "", "Project location on disk")
+	fs.StringVar(&config.RepoUrl, "r", "", "Project remote repository")
+	fs.BoolVar(&config.StaticAssets, "s", false, "Project will have static assets or not")
 
-	// fs := flag.NewFlagSet("scaffold-gen", flag.ContinueOnError)
-	// err := fs.Parse(args)
+	err := fs.Parse(args)
 
-	// if fs.NArg() != 0 {
-	// 	return config, errors.New("No positional parameters expected")
-	// }
-	// return config, err
-	return config
+	if err != nil {
+		return ProjectConfiguration{}, err
+	}
+
+	if fs.NArg() != 0 {
+		return config, errors.New("No positional parameters expected")
+	}
+
+	return config, err
 }
 
 func validateConf(config *ProjectConfiguration) []error {
 	var validationErrors []error
 
-	if len(config.projectName) == 0 {
-		validationErrors = append(validationErrors, errors.New("Project name cannot be empty"))
+	if len(config.Name) == 0 {
+		validationErrors = append(validationErrors, errors.New("Project name cannot be empty\n"))
 	}
-	if len(config.locationOnDisk) ==  0 {
-		validationErrors = append(validationErrors, errors.New("Project location cannot be empty"))
+	if len(config.LocalPath) ==  0 {
+		validationErrors = append(validationErrors, errors.New("Project location cannot be empty\n"))
 	}
-	if len(config.remoteRepoUrl) == 0 {
-		validationErrors = append(validationErrors, errors.New("Project repository URL cannot be empty"))
+	if len(config.RepoUrl) == 0 {
+		validationErrors = append(validationErrors, errors.New("Project repository URL cannot be empty\n"))
 	}
 
 	return validationErrors
 }
 
+func generateScaffold(w io.Writer, conf ProjectConfiguration) {
+	fmt.Fprintf(w, "Generating scaffold for project %s in %s\n", conf.Name, conf.LocalPath)
+}
+
 func main() {
-	config := setupParseFlags(os.Stdout, os.Args[1:])
+	config, err := setupParseFlags(os.Stdout, os.Args[1:])
+
+	if err != nil {
+		fmt.Printf("%s", err.Error())
+		os.Exit(1)
+	}
 	validationErrors := validateConf(&config)
 
 	if (len(validationErrors) > 0) {
 		for _ , error := range validationErrors {
-			fmt.Printf("%s\n", error)
+			fmt.Printf("%s", error)
 		}
 		os.Exit(1)
 	}
+	generateScaffold(os.Stdout, config)
 }
